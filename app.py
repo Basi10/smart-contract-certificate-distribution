eab8cdad459728bfd49a2a74941dc0a7bac6b97b
Basi10 <basilelbirru@gmail.com>
2024-01-10 22:14:35 +0300

    updated the endpoint to interact with flask

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+# app.py
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello from Flask!'}
+    return jsonify(data)
+
+@app.route('/api/option1', methods=['POST'])
+def handle_option1():
+    data = request.get_json()
+    choice = data.get('choice')
+    print(f'Received Option 1 choice: {choice}')
+
+    # Respond with a message
+    return jsonify({'message': f'Received Option 1 choice: {choice}'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
