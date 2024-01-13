0ff69d3e06cbd2c56783b420d07a38462c60c3f9
Basi10 <basilelbirru@gmail.com>
2024-01-13 17:17:22 +0300

    created page for asset creation

---
+++ dapp-frontend/src/components/Asset.tsx
@@ -0,0 +1,13 @@
+// Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h2>Home content goes here</h2>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default Home;
